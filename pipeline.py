9ef7c0c051927cc555900145ba95f7e2693389a3
Derek Zhu <57535849+DerekZZhu@users.noreply.github.com>
2024-10-13 07:07:39 -0700

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -17,19 +17,18 @@ supabase: Client = create_client(
 who = {'9b0041c309fab2f2d507eed2a9051c94': 'Boaz', 'afd06dbb42d96f85c77c269f42347ca4': 'Derek', '7e74b03f1539f93cbb7d6a72016f32a8': 'Ryan'} 
 
 @app.post("/matched")
-async def register_pic(file: UploadFile):
-    # Detect Face
-    file_content = await file.read()
-
-    # Prepare the data and files for the Face++ API
-    files = {'image_file': (file.filename, file_content, file.content_type)}
+async def register_pic(data: dict):
+    image_bytes = bytes(data['image_array'])
+    encoded_image = base64.b64encode(image_bytes).decode('utf-8')
 
     data = {
         'api_key': api_key,
         'api_secret': api_sec,
-        'outer_id': 'FacedataX'
+        'outer_id': 'FacedataX',
+        'image_base64': encoded_image
     }
-    response = requests.post(face_search_url, data=data, files=files)
+    
+    response = requests.post(face_search_url, data=data)
     if response.status_code == 200:
         result = response.json()
         results = result['results'][0]['face_token']
@@ -44,7 +43,6 @@ async def register_pic(file: UploadFile):
     else:
         print("Error")
         print(response.text)
-
 
 @app.get("/album")
 async def album(username: str):
